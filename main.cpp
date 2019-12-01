0d71b4057c2d9106df9938b1bd14db5103804e60
JarvisStark <mrjarviscraft@gmail.com>
2019-12-01 16:38:42 +0300

    Add macros for CLI-reading to main

--- main.cpp
+++ main.cpp
@@ -19,11 +19,19 @@ void stack_repl() noexcept;
 
 void linked_list_repl() noexcept;
 
+#define MAIN_CLI_READ_VALUE_WITH_MESSAGE(type, variable_name, message) cout << message << endl;\
+type variable_name;\
+cin >> variable_name;
+
+#define MAIN_CLI_READ_VALUE(type, variable_name) MAIN_CLI_READ_VALUE_WITH_MESSAGE(\
+type, variable_name, "Enter " << #variable_name\
+);
+
 int main() {
     while (true) {
-        cout << "Enter REPL-mode (`stack` (`s`), `queue` (`q`), `list` (`l`) or `exit` (`e`))" << endl;
-        string mode;
-        cin >> mode;
+        MAIN_CLI_READ_VALUE_WITH_MESSAGE(
+                string, mode, "Enter REPL-mode (`stack` (`s`), `queue` (`q`), `list` (`l`) or `exit` (`e`))"
+        );
         if (mode == "s" || mode == "stack") stack_repl();
         else if (mode == "q" || mode == "queue") queue_repl();
         else if (mode == "l" || mode == "list") linked_list_repl();
@@ -38,24 +46,27 @@ int main() {
 void queue_repl() noexcept {
     Queue<string> queue;
     while (true) {
-        cout << "Enter action (`push`, `pop`, `print` or `exit`)" << endl;
-        string action;
-        cin >> action;
+        MAIN_CLI_READ_VALUE_WITH_MESSAGE(
+                string, action, "Enter action (`push`, `pop`, `size`, `print` or `exit`)"
+        )
 
         if (action == "push") {
-            cout << "Enter value" << endl;
-            string value;
-            cin >> value;
+            MAIN_CLI_READ_VALUE(string, value)
             queue.push(value);
             cout << "Pushed \"" << value << "\"" << endl;
-        } else if (action == "pop") try {
-                const string popped = queue.pop();
-                cout << "Popped " << queue.pop() << endl;
+        } else if (action == "pop") {
+            string popped;
+            try {
+                popped = queue.pop();
             } catch (const out_of_range &e) {
-                cout << "Could not pop a value from an empty queue" << endl;
-            } else if (action == "print") queue.for_each_indexed([](const size_t &index, const string &value) {
-                cout << "[" << index << "] = " << value << endl;
-            });
+                cout << "Could not pop any value: " << e.what() << endl;
+                continue;
+            }
+            cout << "Popped \"" << popped << "\"" << endl;
+        } else if (action == "size") cout << "Size is " << queue.size() << endl;
+        else if (action == "print") queue.for_each_indexed([](const size_t &index, const string &value) {
+            cout << "[" << index << "] = " << value << endl;
+        });
         else if (action == "exit") break;
         else cout << "Unknown action" << endl;
     }
@@ -64,22 +75,25 @@ void queue_repl() noexcept {
 void stack_repl() noexcept {
     Stack<string> stack;
     while (true) {
-        cout << "Enter action (`push`, `pop`, `print` or `exit`)" << endl;
-        string action;
-        cin >> action;
+        MAIN_CLI_READ_VALUE_WITH_MESSAGE(
+                string, action, "Enter action (`push`, `pop`, `size`, `print` or `exit`)"
+        )
 
         if (action == "push") {
-            cout << "Enter value" << endl;
-            string value;
-            cin >> value;
+            MAIN_CLI_READ_VALUE(string, value)
             stack.push(value);
             cout << "Pushed \"" << value << "\"" << endl;
-        } else if (action == "pop") try {
-            const string popped = stack.pop();
-            cout << "Popped " << stack.pop() << endl;
-        } catch (const out_of_range &e) {
-            cout << "Could not pop a value from an empty stack" << endl;
-        } else if (action == "print") stack.for_each_indexed([](const size_t &index, const string &value) {
+        } else if (action == "pop") {
+            string popped;
+            try {
+                popped = stack.pop();
+            } catch (const out_of_range &e) {
+                cout << "Could not pop any value: " << e.what() << endl;
+                continue;
+            }
+            cout << "Popped \"" << popped << "\"" << endl;
+        } else if (action == "size") cout << "Size is " << stack.size() << endl;
+        else if (action == "print") stack.for_each_indexed([](const size_t &index, const string &value) {
             cout << "[" << index << "] = " << value << endl;
         });
         else if (action == "exit") break;
